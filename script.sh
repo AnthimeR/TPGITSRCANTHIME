73614f60cff5379d6ba68e452599b43f751311f9
root <root@ip-172-31-91-210.ec2.internal>
2022-12-06 11:44:32 +0000

    ajout de la creation de user dans le script

--- script.sh
+++ script.sh
@@ -8,7 +8,17 @@ cp ./config__files/ssh/sshd_config /etc/ssh/sshd_config
 cp ./config__files/ssh/Banner /etc/Banner
 
 
-# Redemarrer le service sshd 
+# Redemarrer le service sshd
 
-systemctl restart sshd 
+systemctl restart sshd
+
+
+#Création d'un utilisateur
+
+printf "Création d'un utilisateur. Entrez le nom d'utilisateur désiré :\n"
+
+read user
+
+useradd -m -d/home/$user -s /bin/bash -c "$user" $user && passwd $user && usermod -aG sudo $user && echo -e $user "est devenu un utilisateur root\n"
+
 
